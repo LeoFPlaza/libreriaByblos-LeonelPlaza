6898569f807cc15217b65247138c9ee3244ef75f
LeoFPlaza <leoplaz@gmail.com>
2022-12-08 20:50:47 -0300

    login, finalizacion del carrito en index, fetch del json de las cartas

---
+++ app.js
@@ -0,0 +1,77 @@
+const productGroup = document.querySelector("#productGroup");
+const cardTemplate = document.querySelector("#cardTemplate").content;
+const fragment = document.createDocumentFragment();
+let user = "leoPlaza";
+let password = "Password123";
+let userNav = localStorage.getItem('usuario');
+
+
+const fetchData = async () => {
+  try {
+    const res = await fetch('libros.json');
+    const data = await res.json();
+    cards(data);
+  }catch (err){
+    console.log(err);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+  if (localStorage.getItem('carrito')){
+    carrito = JSON.parse(localStorage.getItem('carrito'));
+    buildCarrito();
+  }
+})
+
+
+const cards = jsonLibros => {
+  jsonLibros.forEach(libro => {
+    console.log(libro);
+    cardTemplate.querySelector(".title").textContent = libro.titulo;
+    cardTemplate.querySelector(".precio").textContent = "$ " + libro.precio;
+    cardTemplate.querySelector(".precio").setAttribute("valor", libro.precio);
+    cardTemplate.querySelector("img").setAttribute("src", libro.cover);
+    cardTemplate.querySelector("img").setAttribute("alt", libro.titulo);
+    cardTemplate.querySelector("img").dataset.id = libro.id;
+
+    const clone = cardTemplate.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  productGroup.appendChild(fragment);
+}
+
+
+//cardHTML();
+function logIn() {
+  let usrInput = document.getElementById("user").value;
+  let passInput = document.getElementById("password").value;
+
+  passInput !== password ? console.log("Usuario: " + usrInput + "\n" + "pass: " + passInput) : this.userNav = localStorage.setItem('userLS', usrInput);
+
+}
+
+hello.on('auth.login', function(auth) {
+
+	// Call user information, for the given network
+	hello(auth.network).api('me').then(function(r) {
+		// Inject it into the container
+		var label = document.getElementById('profile_' + auth.network);
+		if (!label) {
+			label = document.createElement('div');
+			label.id = 'profile_' + auth.network;
+			document.getElementById('profile').appendChild(label);
+		}
+		label.innerHTML = '<img src="' + r.thumbnail + '" /> Hey ' + r.name;
+	});
+});
+
+hello.init({
+	facebook: FACEBOOK_CLIENT_ID,
+	windows: WINDOWS_CLIENT_ID,
+	google: GOOGLE_CLIENT_ID
+}, {redirect_uri: 'redirect.html'});
+
+productGroup.addEventListener("click", (e) => {
+  addCarrito(e);
+});
--- js/modalCarrito.js
+++ js/modalCarrito.js
@@ -1,24 +1,96 @@
-// const modalContainer = document.querySelector(".modalCarrito");
-// const botonComprar = document.querySelector(".buy-btn");
-// const carritoBody = document.querySelector(".modal-dialog");
-// const modalContainer = document.querySelector(".modalCarrito");
+let carrito = {};
 const footerCarrito = document.querySelector("#footerCarrito");
 const itemsCarrito = document.querySelector("#itemsCarrito")
 const templateCarrito = document.querySelector("#templateCarrito").content;
 const templateFooterCarrito = document.querySelector("#templateFooterCarrito").content;
 
+const carritoPush = obj => {
+
+    const libroEnCarrito = {
+        id: obj.querySelector("img").dataset.id,
+        titulo: obj.querySelector(".title").textContent,
+        precio: obj.querySelector(".precio").getAttribute("valor"),
+        cantidad: 1
+    }
+    if (carrito.hasOwnProperty(libroEnCarrito.id)) {
+        libroEnCarrito.cantidad = carrito[libroEnCarrito.id].cantidad + 1;
+    }
+
+    carrito[libroEnCarrito.id] = { ...libroEnCarrito }
+    buildCarrito();
+
+}
+
+
+const addCarrito = (e) => {
+    //console.log(e.target);
+    if (e.target.classList.contains("card-img-top")) {
+        carritoPush(e.target.parentElement);
+    }
+    e.stopPropagation()
+}
+
 const buildCarrito = () => {
-    console.log(carrito);
-    Object.values(carrito).forEach(libroEnCarrito =>{
-        itemsCarrito.innerHTML = '';
+    itemsCarrito.innerHTML = '';
+    // console.log(carrito);
+    Object.values(carrito).forEach(libroEnCarrito => {
+        //console.log(libroEnCarrito)
         templateCarrito.querySelector('th').textContent = libroEnCarrito.id;
         templateCarrito.querySelectorAll('td')[0].textContent = libroEnCarrito.titulo;
         templateCarrito.querySelectorAll('td')[1].textContent = libroEnCarrito.cantidad;
-        templateCarrito.querySelector('span').textContent = libroEnCarrito.cantidad * parseInt(libroEnCarrito.price);
-        templateCarrito.querySelector('.btn-danger').dataset.id = libroEnCarrito.id;
-        
+        templateCarrito.querySelector('span').textContent = libroEnCarrito.cantidad * parseInt(libroEnCarrito.precio);
+        templateCarrito.querySelector('.fa-trash-alt').dataset.id = libroEnCarrito.id;
+
         const cloneProducto = templateCarrito.cloneNode(true);
         fragment.appendChild(cloneProducto);
     })
     itemsCarrito.appendChild(fragment);
+
+    footerTablaCarrito();
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+
 }
+
+const footerTablaCarrito = () => {
+    //footerCarrito.innerHTML = '';
+    const precioTotalCarrito = Object.values(carrito).reduce((accum, { cantidad, precio }) => accum + cantidad * precio, 0);
+
+    Object.keys(carrito).length === 0 ? footerCarrito.innerHTML = ` <th scope="row" colspan="5"> No hay libros en tu carrito</th> ` :
+        footerCarrito.innerHTML = `                                    
+        <th scope="row" colspan="1"> Total</th>
+        <th scope="row" colspan="2"></th>
+        <th scope="row" colspan="1"> $`+ precioTotalCarrito + `</th>
+        <th scope="row" colspan="1"></th>
+        `;
+
+        const vaciarCarrito = document.querySelector(".vaciarCarrito");
+        vaciarCarrito.addEventListener('click', () =>{
+            carrito = {};
+            buildCarrito();
+            // console.log(carrito);
+        })
+
+
+
+    // console.log(precioTotalCarrito);
+}
+
+const btnDelete = e => {
+    // console.log(e.target)
+    if (e.target.classList.contains('fa-trash-alt')){
+        const elementoCarrito = carrito[e.target.dataset.id];
+        elementoCarrito.cantidad--
+
+        if(elementoCarrito.cantidad === 0){
+            delete carrito[e.target.dataset.id];
+        };
+        buildCarrito();
+    }
+    e.stopPropagation();
+
+    //console.log(carrito);
+}
+
+itemsCarrito.addEventListener('click', e => {
+    btnDelete(e);
+})
